b7d568e64edbb17e7aa517b63e03bb71af1a2542
Premraj Patil <ppremraj697@gmail.com>
2024-04-16 19:44:55 +0530

    Completed Navbar(2/2) and solved some challenges associted with Aceternity UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<h1>Hello</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">Hello</h1>
+    </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,8 +19,16 @@ function Navbar({ className }: { className?: string }) {
                 <MenuItem setActive={setActive} active={active} item="Our Courses">
                     <div className="flex flex-col space-y-4 text-sm">
                         <HoveredLink href="/courses">All Courses</HoveredLink>
+                        <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+                        <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+                        <HoveredLink href="/courses">SongWriting</HoveredLink>
+                        <HoveredLink href="/courses">Music Production</HoveredLink>
                     </div>
                 </MenuItem>
+                <Link href={"/contact"}>
+                    <MenuItem setActive={setActive} active={active} item="Contact Us" >
+                    </MenuItem>
+                </Link>
             </Menu>
         </div>
     )
